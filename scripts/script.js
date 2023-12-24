a7cd40ecde053f2c9aeb406870bc39e8e7a41f01
Matvey Nekrasov <matvey1234567@yandex.ru>
2023-12-24 23:36:05 +0300

    добавлены картинки, исправлены замечания

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 const salesList = [
   {
-    image: ["../images/test.png", "../images/shop.png"],
+    image: ["../images/test.png", "../images/shop.png", "../images/shop.png"],
     title: "CLARINS Комплексная омолаживающая двойная сыворотка Double Serum",
     desc: "Lörem ipsum sulig mokyr i ryggplankning. Social distansering fotokrati. Linade sor jag gysertad. Laning spertad, plang, och analiga.  Hypodatt vir prehet multirat. Dong sejönat bibel. Suprabel dipav i löjöl saligisk. Cli-fi vivarade sosa trafficking. Sena jöst. Past tidaling kontrartad, lalövis kon. Konar fomo. Lak avis feskap valpromenera jöktig. Digyligen megament hill. Sotesk difasont. Sverka paravis, den epitopi povide. Jölask geotår minidator",
-    salePrice: 20000,
-    price: 30000,
+    salePrice: "20 000 ₽",
+    price: "30000 ₽",
     sale: "90%",
     shopImg: "../images/shop.png",
     shopName: "Лэтуаль",
@@ -13,20 +13,20 @@ const salesList = [
     image: ["../images/test.png", "../images/shop.png"],
     title: "CLARINS Комплексная омолаживающая двойная сыворотка Double Serum",
     desc: "Lörem ipsum sulig mokyr i ryggplankning. Social distansering fotokrati. Linade sor jag gysertad. Laning spertad, plang, och analiga.  Hypodatt vir prehet multirat. Dong sejönat bibel. Suprabel dipav i löjöl saligisk. Cli-fi vivarade sosa trafficking. Sena jöst. Past tidaling kontrartad, lalövis kon. Konar fomo. Lak avis feskap valpromenera jöktig. Digyligen megament hill. Sotesk difasont. Sverka paravis, den epitopi povide. Jölask geotår minidator",
-    salePrice: 2000,
-    price: 3000,
+    salePrice: "2000 ₽",
+    price: "3000 ₽",
     sale: "90%",
     shopImg: "../images/shop.png",
     shopName: "Лэтуаль",
   },
   {
-    image: ["../images/test.png", "../images/shop.png"],
+    image: ["dsggs"],
     title: "CLARINS Комплексная омолаживающая двойная сыворотка Double Serum",
     desc: "Тесты тесты тесты",
     salePrice: 25500,
     price: 412400,
     sale: "90%",
-    shopImg: "../images/shop.png",
+    shopImg: "sdgsd",
     shopName: "Лэтуаль",
   },
 ];
@@ -41,46 +41,69 @@ function createHtmlSalesCatalog(salesList) {
       htmlCatalog += `
         <li class="card-list__item">
           <article class="card" data-index="${index}">
-            <div class="card__image-wrapper">
-              <img
-                src="${image[0]}"
-                alt="Карточка товара"
-                class="card__image"
-              />
-              <div class="card__image-slider-wrapper">
-                <div class="card__image-slider is-active"></div>
-                <div class="card__image-slider"></div>
-                <div class="card__image-slider"></div>
-              </div>
-            </div>
-            <h2 class="card__title">
-              ${title}
-            </h2>
-            <p class="card__text">
-              ${desc}
-            </p>
-            <div class="card__prices">
-              <p class="card__sale-price">${salePrice}</p>
-              <p class="card__price">${price}</p>
-              <p class="card__sale">${sale}</p>
-            </div>
-            <div class="card__shop-info">
-              <img
-                src="${shopImg}"
-                alt="Иконка магазина"
-                class="card__shop-image"
-              />
-              <p class="card__shop-name">${shopName}</p>
-            </div>
-            <button type="button" class="card__favorite">
-              <img src="./svg/bookmark.svg" alt="Поместить в закладки" />
-            </button>
-          </article>
+            <div class="card__image-wrapper">`;
+
+      if (image.length == 0) {
+        htmlCatalog += `
+          <img
+            src="../images/no-image.jpg"
+            alt="Карточка товара"
+            class="card__image"
+          />
+        `;
+      } else if (image.length == 1) {
+        htmlCatalog += `
+          <img
+            src="${image[0]}"
+            alt="Карточка товара"
+            class="card__image"
+          />
+        `;
+      } else {
+        htmlCatalog += `
+          <img
+            src="${image[0]}"
+            alt="Карточка товара"
+            class="card__image"
+          />
+          <div class="card__image-slider-wrapper">
+            <div class="card__image-slider is-active"></div>
+        `;
+        for (let i = 1; i < image.length; i++) {
+          htmlCatalog += `<div class="card__image-slider"></div>`;
+        }
+        htmlCatalog += `</div>`;
+      }
+
+      htmlCatalog += `
+        </div>
+        <h2 class="card__title">
+          ${title}
+        </h2>
+        <p class="card__text">
+          ${desc}
+        </p>
+        <div class="card__prices">
+          <p class="card__sale-price">${salePrice}</p>
+          <p class="card__price">${price}</p>
+          <p class="card__sale">${sale}</p>
+        </div>
+        <div class="card__shop-info">
+          <img
+            src="${shopImg}"
+            alt="Иконка магазина"
+            class="card__shop-image"
+          />
+          <p class="card__shop-name">${shopName}</p>
+        </div>
+        <button type="button" class="card__favorite">
+          <img src="./svg/bookmark.svg" alt="Поместить в закладки" />
+        </button>
+        </article>
         </li>
       `;
     }
   );
-
   return htmlCatalog;
 }
 
@@ -94,9 +117,7 @@ const sales = document.querySelectorAll(".card");
 if (sales) {
   sales.forEach((el) => {
     let currentSale = el;
-
     const sliderDots = el.querySelectorAll(".card__image-slider");
-    console.log(sliderDots);
 
     sliderDots.forEach((el, index) => {
       el.setAttribute("data-index", index);
@@ -109,7 +130,6 @@ if (sales) {
             `.card__image-slider[data-index="${e.currentTarget.dataset.index}"]`
           )
           .classList.add("is-active");
-        console.log(currentSale.dataset.index);
         currentSale
           .querySelector(".card__image")
           .setAttribute(
@@ -122,3 +142,11 @@ if (sales) {
     });
   });
 }
+
+const images = document.querySelectorAll(".card__image, .card__shop-image");
+images.forEach((element) => {
+  element.setAttribute("loading", "lazy");
+  element.addEventListener("error", (e) => {
+    e.currentTarget.setAttribute("src", "../images/no-image.jpg");
+  });
+});
